02da77bc8c2a67f18e08795016e63c79cabc16cf
Jonathan Welton <j.welton@audionetwork.com>
2017-07-24 15:06:16 +0100

    Added a basic React composer app template

---
+++ ANW.ComposerApp/webpack.config.js
@@ -0,0 +1,19 @@
+﻿var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './Scripts/react-composer-app/App.jsx',
+    output: { path: path.resolve(__dirname, "Scripts/react-composer-app/compiled"), filename: 'bundle.js' },
+    module: {
+        loaders: [
+          {
+              test: /.jsx?$/,
+              loader: 'babel-loader',
+              exclude: /node_modules/,
+              query: {
+                  presets: ['es2015', 'react']
+              }
+          }
+        ]
+    },
+};
