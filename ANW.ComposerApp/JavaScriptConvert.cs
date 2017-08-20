1530832a91147261a2f991df782a55a317fa937b
David Lewis <david.lewis@wayveapp.com>
2017-08-20 21:51:18 +0100

    Serialize ComposerRepo.Composers into a JavaScript Literal

---
+++ ANW.ComposerApp/JavaScriptConvert.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Serialization;
+
+namespace ANW.ComposerApp
+{
+    public static class JavaScriptConvert
+    {
+        public static IHtmlString SerializeObject(object value)
+        {
+            using (var stringWriter = new StringWriter())
+            using (var jsonWriter = new JsonTextWriter(stringWriter))
+            {
+                var serializer = new JsonSerializer
+                {
+                    // Let's use camelCasing as is common practice in JavaScript
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                };
+
+                // We don't want quotes around object names
+                jsonWriter.QuoteName = false;
+                serializer.Serialize(jsonWriter, value);
+
+                return new HtmlString(stringWriter.ToString());
+            }
+        }
+    }
+}
